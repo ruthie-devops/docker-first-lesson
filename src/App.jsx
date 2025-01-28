349d47ce89928439eb39f6992e0dd235b4138267
אליעזרוב שיינדל לאה <  @mbjcomp.local>
2025-01-28 19:55:18 +0200

    smile

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>😍😊😘😜😃</h1>
+      <h1>😍😊😘😜</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
